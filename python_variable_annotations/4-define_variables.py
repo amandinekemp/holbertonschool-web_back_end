607cd901b6e186d3e35f0b085180b320146326b2
DDoudine <amandinekemp@gmail.com>
2024-04-22 18:20:03 +0200

    Fix pycodestyle

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Defines and annotates four variables: 'a', 'pi', 'i_understand_annotations',
+and 'school'.
+"""
 
 a: int = 1  # a is an integer with a value of 1
 pi: float = 3.14  # pi is a float with a value of 3.14
