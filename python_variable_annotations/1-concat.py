b0493b7043579436e94b3a9859065c9065552935
DDoudine <amandinekemp@gmail.com>
2024-04-22 18:03:14 +0200

    Add doc

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -3,5 +3,13 @@
 
 
 def concat(str1: str, str2: str) -> str:
-	"""concatenate two strings"""
-	return str1 + str2
+    """Concatenates two strings
+
+    Args:
+        str1 (str): First string to concatenate
+        str2 (str): Second string to concatenate
+
+    Returns:
+        str: Resulting string from the concatenation of str1 and str2
+    """
+    return str1 + str2
