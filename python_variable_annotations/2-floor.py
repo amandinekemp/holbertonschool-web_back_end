a2040d978f4c169df4ad90888356f29b9200c253
DDoudine <amandinekemp@gmail.com>
2024-04-22 18:08:19 +0200

    Add function floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""floor"""
+
+
+def floor(n: float) -> int:
+    """Floor function
+
+    Args:
+            n (float): Number to be floored
+
+    Returns:
+            int: Floored number
+    """
+    return int(n)
