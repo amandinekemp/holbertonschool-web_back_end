72c60601d2a66e818082f9fc65e9a473eb6c57aa
DDoudine <amandinekemp@gmail.com>
2024-04-22 18:05:55 +0200

    Add doc

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,5 +3,13 @@
 
 
 def add(a: float, b: float) -> float:
-	"""add two floats"""
-	return a + b
+    """Addition
+
+    Args:
+        a (float): First number to add
+        b (float): Second number to add
+
+    Returns:
+        float: Result of adding a and b
+    """
+    return a + b
