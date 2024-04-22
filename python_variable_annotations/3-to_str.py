a6a05cbeb249e660324cf094bad61bf0294046b5
DDoudine <amandinekemp@gmail.com>
2024-04-22 18:12:43 +0200

    Add function to_str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""to_str"""
+
+
+def to_str(n: float) -> str:
+    """Converts a float to a string
+
+    Args:
+            n (float): Number to convert
+
+    Returns:
+            str: String representation of the number
+    """
+    return str(n)
