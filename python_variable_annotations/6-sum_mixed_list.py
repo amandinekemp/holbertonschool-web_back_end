5b2c2199ed4b1ffc5e134c99491f0394968c0a54
DDoudine <amandinekemp@gmail.com>
2024-04-23 08:22:39 +0200

    Fix tabulation

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -8,10 +8,10 @@ def sum_mix_list(input_list: List[Union[int, float]]) -> Union[int, float]:
     """Sums a list of floats and ints
 
     Args:
-                                    input_list (List[Union[int, float]]):
-                                    List of floats and ints to sum
+        input_list (List[Union[int, float]]):
+        List of floats and ints to sum
 
     Returns:
-                                    Union[int, float]: Sum of the input_list
+        Union[int, float]: Sum of the input_list
     """
     return sum(input_list)
