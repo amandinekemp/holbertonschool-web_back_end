95642b5938e97d6d918f6476c905a7fd1f0b70ff
DDoudine <amandinekemp@gmail.com>
2024-04-26 09:36:36 +0200

    Fix pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,6 +6,7 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """Asynchronously wait 1 second, then yield a random number
     between 0 and 10.
