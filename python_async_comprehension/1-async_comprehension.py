d026866aa6162fb70022491bbb0f9a8ba2a5e058
DDoudine <amandinekemp@gmail.com>
2024-04-26 09:40:14 +0200

    Add annotation coroutine and name

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,12 @@
 on async_generator"""
 
 import asyncio
+from typing import List
 
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_generator():
+async def async_comprehension() -> List[float]:
     """
     Collects 10 random numbers using async comprehension over async_generator.
 
