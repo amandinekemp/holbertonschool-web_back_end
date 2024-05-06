b971accdd192d4b90a1f89406e9388cb14d4b70c
DDoudine <amandinekemp@gmail.com>
2024-05-06 11:19:44 +0200

    Delete Doc

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,21 +1,12 @@
-/**
-* Returns a string declared with 'const'.
-*/
 export function taskFirst() {
   const task = 'I prefer const when I can.'
   return task
 }
 
-/**
-* Returns a static string.
-*/
 export function getLast() {
   return ' is okay'
 }
 
-/**
-* Returns a string declared with 'let'.
-*/
 export function taskNext() {
   let combination = 'But sometimes let'
   combination += getLast()
