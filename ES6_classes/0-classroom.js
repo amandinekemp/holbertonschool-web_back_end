5a996036e47af57c3dafe2147063fdc7115d8053
DDoudine <amandinekemp@gmail.com>
2024-05-07 08:46:45 +0200

    Add ClassRoom class with max students size property

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+// Class representing a classroom
+export default class ClassRoom {
+  // Class constructor
+  constructor(maxStudentsSize) {
+    // Assign the maximum number of students to the _maxStudentsSize property
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
