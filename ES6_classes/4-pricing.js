2369a16eb79ddaaec5438612a0f9ea242bc5b8db
DDoudine <amandinekemp@gmail.com>
2024-05-08 08:10:58 +0200

    Add Pricing class with getters, setters, and full price display and conversion methods

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,49 @@
+// Importing the Currency class
+import Currency from './3-currency.js';
+
+// Declaring the Pricing class
+export default class Pricing {
+  // Declaring the constructor
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a Number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a currency');
+    }
+
+    // Initializing the attributes with an underscore
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter for the amount attribute
+  get amount() {
+    return this._amount;
+  }
+
+  // Setter for the amount attribute
+  set amount(value) {
+    this._amount = value;
+  }
+
+  // Getter for the currency attribute
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter for the currency attribute
+  set currency(value) {
+    this._currency = value;
+  }
+
+  // Method to display the full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  // Static method to convert a price
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
