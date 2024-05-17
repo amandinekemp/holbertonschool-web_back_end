f21947bf6d7a7686a790d81a8d82b59c81454c7a
DDoudine <amandinekemp@gmail.com>
2024-05-17 23:28:01 +0200

    Fix tab in space (requierement)

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,17 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 const handleProfileSignup = async (firstName, lastName, fileName) => {
-	const result = [];
-	try {
-		const user = await signUpUser(firstName, lastName);
-		result.push({ status: 'fulfilled', value: user });
-		await uploadPhoto(fileName);
-	} catch (error) {
-		result.push({
-			status: 'rejected', value: error.toString(),
-		});
-	}
-	return result;
+  const result = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    result.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (error) {
+    result.push({
+      status: 'rejected', value: error.toString(),
+    });
+  }
+  return result;
 };
 
 export default handleProfileSignup;
